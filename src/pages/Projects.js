c1eff431f2ed35cc4dc2fdab1afd649d7e6f12a8
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-12 18:33:33 -0400

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,9 +3,17 @@ import { Link } from 'react-router-dom';
 export default function Projects() {
 
   return (
-    <div>
+    <div className="text-center">
      <h1>Project Page</h1>
+     <br></br>
+      <h2>Auto Grocery Sorter</h2>
+      <h3>Full Stack App using MERN</h3>
+      <br></br>
+     
+      <img className = "project"src = "https://github.com/MERNMONGO/Store/raw/main/Landing.png "></img>
+      <Link to = "https://grocer-frontend.onrender.com/"> <p>Try it here!</p></Link>
       
+      <Link to = "https://github.com/MERNMONGO/Store"> <p>Contribute towards code</p></Link>
     </div>
   );
 }
