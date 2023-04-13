42cc0ebd75e2a7d89f712f145d750fc7fba834ef
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-13 15:47:24 -0400

    Added Footer

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-bottom ">
+
+
+  
+    
+    
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link"to="/">Contact</Link>
+        </li>
+       
+      </ul>
+    
+
+</nav>
+  );
+}
+
+export default Footer;
