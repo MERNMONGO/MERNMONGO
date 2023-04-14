7674cb04d03b8790dc72d5de215078ab1cd71c48
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-13 22:58:50 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ export default function About() {
     return (
      <div className = "text-center">
      <h1>About</h1>
-     <h2>Showcase of my Work</h2>
+     <h2>This portfolio acts as showcase of my Work</h2>
      <p>Full Stack Software Developer</p>
      </div>
   );
