3e293e741edc40e1d31691e1b8b515aed6c2ab28
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-13 22:55:49 -0400

    Contact Route added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Navbar from './pages/components/Navbar';
 import About from './pages/About';
 import Projects from './pages/Projects';
-
+import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
@@ -16,6 +16,7 @@ function App() {
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
