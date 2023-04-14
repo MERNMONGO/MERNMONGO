a9753827a903a1ece32ccf0336633734c4d4eed2
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-13 22:56:18 -0400

    Contact Content Updated

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+
+export default function Contact() {
+  
+    return (
+     <div className = "text-center">
+     <h1>Contact Info</h1>
+
+     </div>
+  );
+}
